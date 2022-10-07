d0805af4110601c2ee491151b9ae8561861e6d81
MaheshWadhe03 <97393826+MaheshWadhe03@users.noreply.github.com>
2022-10-07 11:01:49 +0530

    Update add.java

--- java/add.java
+++ java/add.java
@@ -1,5 +1,8 @@
 public class add{
       public static void main(String args[]){
-            System.out.println("HI");
+           //System.out.println("HI");
+             int n1=12, n2=11, sum; 
+             sum=n1+n2;
+             System.out.println("The sum of numbers is:"+sum);
       }
 }
